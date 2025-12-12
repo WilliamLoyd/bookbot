467f24ebf5fed738786ba4cf2effccfb520ae764
WilliamLoyd <info@twconsulting.biz>
2025-12-12 10:49:05 +0000

    refactor stats.py and start on character count

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
+from stats import get_num_words, character_count
+def main():
+    path_to_file = "books/frankenstein.txt"
+    text = get_book_text(path_to_file).split()
+    counter = get_num_words(text)
+    print(f"Found {counter} total words")
+        
+        
+
+
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
         return file_contents
 
-def main():
-    path_to_file = "books/frankenstein.txt"
-    text = get_book_text(path_to_file)
-    print(text)
+
     
 
 main()
---
+++ stats.py
@@ -0,0 +1,15 @@
+def get_num_words(text):
+    counter = len(text)
+    return counter
+
+def character_count(text):
+    characters = text.lower()
+    amount = 0
+    character_dict = {}
+    for i in characters:
+        if i in character_dict == False:
+            character_dict.add(i)
+        
+        character_dict[character_list[0]] = 
+    return character_dict
+        
